6f1c5f8d89517f8d11d0f86204012cbfab199579
Marzio95 <marziodellarocca14@gmail.com>
2022-05-21 13:54:10 +0200

    sistemo la navbar e la sua estetica

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,14 +1,21 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('homepage') }}">First-Auth</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse my_navbar_posts" id="navbarNav">
+            <ul class="navbar-nav my_ul_nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.posts.create') }}">Create Post</a>
+                    <a class="nav-link active" href="{{ route('admin.posts.create') }}">Create Post</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('admin.posts.index') }}">Listing
+                        Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('admin.myposts') }}">My Posts</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{ route('admin.home') }}">Login Page</a>
